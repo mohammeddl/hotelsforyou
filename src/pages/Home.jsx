5daac9ff15494606c08af265de7bb98683507b95
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-05-05 19:46:47 +0100

    add banenr

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import Discover from "../components/Home/Discover";
 import Search from "../components/Home/Search";
 import Destination from "../components/Home/Destination";
 import Rooms from "../components/Home/Rooms";
+import Banner from "../components/Home/Banner";
 
 function Home() {
   return (
     <>
     <Search/>
+    <Banner/>
     <Discover/>
     <Destination/>
     <Rooms/>
